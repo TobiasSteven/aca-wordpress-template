4d1130735148fc0d9f6e43272031422b42c170f0
Abdoulaye BARRY <barryab12@gmail.com>
2025-06-11 16:12:16 +0000

    fix: update asset version and refine styles for featured post block

--- blocks/featured-post/build/index.asset.php
+++ blocks/featured-post/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '9f4144ec194dbe8e3520');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '6c393742c50bd2d85217');
